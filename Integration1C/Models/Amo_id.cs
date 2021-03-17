71d331f139e83e3121d6683b80f5a6cdd324f873
u705708 <u705708@gmail.com>
2021-03-18 01:38:50 +0500

    Some cleaning

--- Integration1C/Models/Amo_id.cs
+++ Integration1C/Models/Amo_id.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Integration1C
+﻿namespace Integration1C
 {
     public class Amo_id
     {
