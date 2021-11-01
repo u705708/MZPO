05f00ad6e22069157ba80415929af80b0f0f9739
u705708 <u705708@gmail.com>
2021-11-01 20:17:58 +0500

    Added two new managers

--- MZPO/Controllers/ReportsController.cs
+++ MZPO/Controllers/ReportsController.cs
@@ -73,7 +73,7 @@ namespace MZPO.Controllers.ReportProcessors
         #endregion
 
         #region CorporateSales
-        // GET reports/corporatesales/1630443600,1633035599
+        // GET reports/corporatesales/1633035600,1635713999
         [ActionName("CorporateSales")]
         [HttpGet("{from},{to}")]                                                                                                                //Запрашиваем отчёт для диапазона дат
         public IActionResult CorporateSales(string from, string to)
